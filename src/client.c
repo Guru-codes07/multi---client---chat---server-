00a80626ad8ecd448b078e123987e966aa6da008
Guru-codes07 <2020gurup@gmail.com>
2026-06-26 21:23:52 +0530

    error correction in client.c

--- src/client.c
+++ src/client.c
@@ -27,7 +27,7 @@ void* receive_messages(void* arg) {
         buffer[byte_received] = '\0';
         
         // Print whatever background message came in (notifications or text from others)
-        printf("%s", buffer);
+        printf("\r%s", buffer);
         printf("you: "); 
         fflush(stdout);
     }
